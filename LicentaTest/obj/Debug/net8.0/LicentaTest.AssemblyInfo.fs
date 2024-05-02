7551061fb972092a3bbcd8b6c328f2d4fdc1d479
danielrotarmihai@gmail.com <danielrotarmihai@gmail.com>
2024-05-02 11:18:32 +0300

    commit azi dimineata

--- LicentaTest/obj/Debug/net8.0/LicentaTest.AssemblyInfo.fs
+++ LicentaTest/obj/Debug/net8.0/LicentaTest.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("LicentaTest")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c1a5bd6e8619803c32bcac41d7e9fd9a82c68d8")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+371b495522285d064bbef810991cd1a8cbe5b4e5")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("LicentaTest")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("LicentaTest")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
